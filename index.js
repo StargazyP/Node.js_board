4b97ca5cac97a62dd94efa4046d26da6615aae70
takanozomi <jdajsl0415@naver.com>
2023-04-12 02:23:31 +0900

    server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/home.html')
+})
+
+app.listen(8080, function() {
+    console.log('server open');
+})
+
+app.get('/write', function(req,res){
+    res.sendFile(__dirname + '/write.html')
+})
