188a816ad517cc1461f1e64cfa9ed98b6e1a744f
takazonomi <jdajsl0415@naver.com>
2023-05-01 15:03:52 +0900

    db

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+const MongoClient = require('mongodb').MongoClient;
+var db;
+MongoClient.connect('mongodb+srv://jdajsl0415:blackser7789@cluster0.wxlph6a.mongodb.net/test', function(req,res){
+    if(req) return console.log("에러")
+
+    db = client.db('server');
+
+    db.collection('post').insertOne({이름 : 'jone', _id : 20}, function(req,res){
+        console.log("저장완료");
+    });
+
+    app.listen(8080, function(){
+        console.log("listening 8080")
+    });
+});
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/', function(req,res){
