174f37597492a858bebc075e43792c3cf7cbadeb
takazonomi <jdajsl0415@naver.com>
2023-05-02 11:44:27 +0900

    dbsettingend

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ const MongoClient = require('mongodb').MongoClient;
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }));
 
 var db;
-MongoClient.connect('mongodb+srv://jdajsl0415:blackser7789@cluster0.wxlph6a.mongodb.net/test', function(req,res){
-    if(req) console.log("에러")
-
-    db = client.db('mentoling');
-
-    app.listen(8080, function(){
-        console.log("listening 8080")
-    });
+MongoClient.connect('mongodb+srv://jdajsl0415:blackser7789@cluster0.wxlph6a.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, function(err, client) {
+    if (err) console.log(err);
+    else {
+        db = client.db('server');
+        console.log('db open');
+    }
 });
 
 
@@ -26,9 +24,7 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/home.html')
 })
 
-app.listen(8080, function() {
-    console.log('server open');
-})
+
 
 app.get('/write', function(req,res){
     res.sendFile(__dirname + '/write.html')
@@ -39,8 +35,16 @@ app.get('/list', function(req,res){
 })
 
 app.post('/add', function(req,res){
-    res.send('전송완료');
-    db.collection('post').insertOne({ 제목 : req.body.date, 날짜 : req.body.title }, function(req,res){
+    if (!db) {
+        console.log('DB 연결 오류');
+        return;
+    }
+    db.collection('post').insertOne({ 제목 : req.body.title, 날짜 : req.body.date }, function(){
         console.log('저장완료');
     });
+    res.send('전송완료');
 });
+
+app.listen(8080, function() {
+    console.log('server open');
+})
