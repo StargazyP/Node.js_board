9a3490c092a49d41370b6018e1ab756709f9b712
takazonomi <jdajsl0415@naver.com>
2023-05-02 10:49:59 +0900

    db0502

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const MongoClient = require('mongodb').MongoClient;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true})) 
+
 var db;
 MongoClient.connect('mongodb+srv://jdajsl0415:blackser7789@cluster0.wxlph6a.mongodb.net/test', function(req,res){
-    if(req) return console.log("에러")
+    if(req) console.log("에러")
 
-    db = client.db('server');
-
-    db.collection('post').insertOne({이름 : 'jone', _id : 20}, function(req,res){
-        console.log("저장완료");
-    });
+    db = client.db('mentoling');
 
     app.listen(8080, function(){
         console.log("listening 8080")
     });
 });
-app.use(bodyParser.urlencoded({extended : true}));
+
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/home.html')
@@ -38,9 +39,7 @@ app.get('/list', function(req,res){
 })
 
 app.post('/add', function(req,res){
-    req.send('전송완료');
-    console.log(req.body.date);
-    console.log(req.body.title);
+    res.send('전송완료');
     db.collection('post').insertOne({ 제목 : req.body.date, 날짜 : req.body.title }, function(req,res){
         console.log('저장완료');
     });
